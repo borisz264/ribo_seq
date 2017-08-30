82db513af91bda5a09a0e3157ea1163fe599e841
borisz264 <borzin264@gmail.com>
2017-08-30 12:48:11 -0400

    changed "get_all_fragment_length_counts()" to "get_all_CDS_fragment_length_counts()" added checks that transcripts are coding for several plots removed plot_second_stop_codon_average() added skeleton for codon_metaplots(). Need to write implementation.

--- ribo_plotting.py
+++ ribo_plotting.py
@@ -19,7 +19,7 @@ def plot_fragment_length_distributions(experiment, min_x=15):
     for lib in experiment.libs:
         plot = fig.add_subplot(num_plots_high, num_plots_wide, plot_index+1)
         sample_name = lib.lib_settings.sample_name
-        fragment_length_counts = lib.get_all_fragment_length_counts()
+        fragment_length_counts = lib.get_all_CDS_fragment_length_counts()
         bins = range(0, max(fragment_length_counts.keys()))
         fragment_length_fractions = np.array([fragment_length_counts[length] for length in bins]) / float(
             lib.total_mapped_fragments)
@@ -54,9 +54,9 @@ def plot_readthrough_box(experiment, log = False):
         readthroughs = [tx.compute_readthrough_ratio(16, read_end='5p', read_lengths='all', cds_cutoff=128,
                                     log=log, post_cds_start_buffer=12, pre_cds_stop_buffer=15,
                                     pre_extension_stop_buffer=15, post_cds_stop_buffer=9) for
-                        tx in lib.transcripts.values() if not tx.compute_readthrough_ratio(16, read_end='5p', read_lengths='all', cds_cutoff=128,
+                        tx in lib.transcripts.values() if (not tx.compute_readthrough_ratio(16, read_end='5p', read_lengths='all', cds_cutoff=128,
                                     log=log, post_cds_start_buffer=12, pre_cds_stop_buffer=15,
-                                    pre_extension_stop_buffer=15, post_cds_stop_buffer=9) == None ]
+                                    pre_extension_stop_buffer=15, post_cds_stop_buffer=9) == None) and tx.is_coding ]
         data.append(readthroughs)
         legends.append('%s (%d)' % (sample_name, len(readthroughs)))
         # note that all but the last bin exclude the right (larger) edge of the bin. So I add an extra bin.
@@ -95,7 +95,8 @@ def plot_frame_distributions(experiment, read_lengths = ['all', [28], [29], [30]
                 offsets = {'5p':-15, '3p': 15}
                 offset = offsets[read_end]
                 for transcript in lib.transcripts.values():
-                    frame_counts = frame_counts + transcript.get_read_frame_counts(transcript.cds_start+offset, transcript.cds_end+offset, read_end=read_end, read_lengths=read_length)
+                    if transcript.is_coding:
+                        frame_counts = frame_counts + transcript.get_read_frame_counts(transcript.cds_start+offset, transcript.cds_end+offset, read_end=read_end, read_lengths=read_length)
 
                 # note that all but the last bin exclude the right (larger) edge of the bin. So I add an extra bin.
                 bar_corners = (np.arange(3)+0.25)*.5
@@ -135,18 +136,19 @@ def plot_start_codon_average(experiment, up = 100, down = 500, min_cds_reads = 1
         normed_count_sum = np.zeros(down+up+1)
         inclusion_sum = np.zeros(down + up + 1)
         for transcript in lib.transcripts.values():
-            if read_end == '5p':
-                start_offset = -15
-                stop_offset = -12
-            elif read_end == '3p':
-                start_offset = 14
-                stop_offset = 18
-            cds_reads = transcript.get_cds_read_count(start_offset, stop_offset, read_end=read_end, read_lengths=read_lengths)
-            if cds_reads >= min_cds_reads:
-                tx_count, tx_inclusion = transcript.get_read_counts_array(transcript.cds_start, -1*up, down, read_end=read_end,
-                                                                read_lengths=read_lengths)
-                normed_count_sum += tx_count/(float(cds_reads)/transcript.cds_length)
-                inclusion_sum += tx_inclusion
+            if transcript.is_coding:
+                if read_end == '5p':
+                    start_offset = -15
+                    stop_offset = -12
+                elif read_end == '3p':
+                    start_offset = 14
+                    stop_offset = 18
+                cds_reads = transcript.get_cds_read_count(start_offset, stop_offset, read_end=read_end, read_lengths=read_lengths)
+                if cds_reads >= min_cds_reads:
+                    tx_count, tx_inclusion = transcript.get_CDS_read_counts_array(transcript.cds_start, -1 * up, down, read_end=read_end,
+                                                                                  read_lengths=read_lengths)
+                    normed_count_sum += tx_count/(float(cds_reads)/transcript.cds_length)
+                    inclusion_sum += tx_inclusion
         nt_positions = np.arange(-1*up, down+1)-0.5
         plot.bar(nt_positions, normed_count_sum/inclusion_sum,
                   color=colormap((plot_index - 1) / float(len(experiment.libs))), lw=0, label=sample_name)
@@ -180,11 +182,12 @@ def plot_second_stop_positions(experiment, up = 500, down = 100, min_cds_reads =
         plot = fig.add_subplot(num_plots_high, num_plots_wide, plot_index + 1)
         second_stop_positions = []
         for transcript in lib.transcripts.values():
-            cds_reads = transcript.get_cds_read_count(-15, 12, read_end=read_end, read_lengths=read_lengths)
-            if cds_reads >= min_cds_reads:
-                second_stop = transcript.second_stop_position()
-                if not second_stop == None:
-                    second_stop_positions.append(second_stop+3-transcript.cds_end)
+            if transcript.is_coding:
+                cds_reads = transcript.get_cds_read_count(-15, 12, read_end=read_end, read_lengths=read_lengths)
+                if cds_reads >= min_cds_reads:
+                    second_stop = transcript.second_stop_position()
+                    if not second_stop == None:
+                        second_stop_positions.append(second_stop+3-transcript.cds_end)
         bins = range(-10, 200)
         bins.append(1000000)
         hist, bin_edges = np.histogram(second_stop_positions, bins)
@@ -229,18 +232,19 @@ def plot_stop_codon_average(experiment, up = 500, down = 100, min_cds_reads = 12
         normed_count_sum = np.zeros(down+up+1)
         inclusion_sum = np.zeros(down + up + 1)
         for transcript in lib.transcripts.values():
-            if read_end == '5p':
-                start_offset = -15
-                stop_offset = -12
-            elif read_end == '3p':
-                start_offset = 14
-                stop_offset = 18
-            cds_reads = transcript.get_cds_read_count(start_offset, stop_offset, read_end=read_end, read_lengths=read_lengths)
-            if cds_reads >= min_cds_reads:
-                tx_count, tx_inclusion = transcript.get_read_counts_array(transcript.cds_end, -1*up, down, read_end=read_end,
-                                                                read_lengths=read_lengths)
-                normed_count_sum += tx_count/(float(cds_reads)/transcript.cds_length)
-                inclusion_sum += tx_inclusion
+            if transcript.is_coding:
+                if read_end == '5p':
+                    start_offset = -15
+                    stop_offset = -12
+                elif read_end == '3p':
+                    start_offset = 14
+                    stop_offset = 18
+                cds_reads = transcript.get_cds_read_count(start_offset, stop_offset, read_end=read_end, read_lengths=read_lengths)
+                if cds_reads >= min_cds_reads:
+                    tx_count, tx_inclusion = transcript.get_CDS_read_counts_array(transcript.cds_end, -1 * up, down, read_end=read_end,
+                                                                                  read_lengths=read_lengths)
+                    normed_count_sum += tx_count/(float(cds_reads)/transcript.cds_length)
+                    inclusion_sum += tx_inclusion
         nt_positions = np.arange(-1*up, down+1)-0.5
         plot.bar(nt_positions, normed_count_sum/inclusion_sum,
                   color=colormap((plot_index - 1) / float(len(experiment.libs))), lw=0, label=sample_name)
@@ -262,65 +266,6 @@ def plot_stop_codon_average(experiment, up = 500, down = 100, min_cds_reads = 12
     plt.savefig(out_name, transparent='True', format='pdf')
     plt.clf()
 
-
-
-def plot_second_stop_codon_average(experiment, up = 500, down = 100, min_cds_reads = 128, read_end='5p', read_lengths='all'):
-    '''
-    line up reads at first in-frame stop after canonical stop codon
-    :param experiment:
-    :param up:
-    :param down:
-    :param min_cds_reads:
-    :param read_end:
-    :param read_lengths:
-    :return:
-    '''
-    num_libs = len(experiment.libs)
-    num_plots_wide = 1
-    num_plots_high = num_libs
-    fig = plt.figure(figsize=(8, 2*num_libs))
-    colormap = uniform_colormaps.viridis
-    plot_index = 0
-    for lib in experiment.libs:
-        plot = fig.add_subplot(num_plots_high, num_plots_wide, plot_index + 1)
-        sample_name = lib.lib_settings.sample_name
-        normed_count_sum = np.zeros(down+up+1)
-        inclusion_sum = np.zeros(down + up + 1)
-        for transcript in lib.transcripts.values():
-            if read_end == '5p':
-                start_offset = -15
-                stop_offset = -12
-            elif read_end == '3p':
-                start_offset = 14
-                stop_offset = 18
-            cds_reads = transcript.get_cds_read_count(start_offset, stop_offset, read_end=read_end, read_lengths=read_lengths)
-            if cds_reads >= min_cds_reads:
-                second_stop = transcript.second_stop_position()
-                if not second_stop == None:
-                    tx_count, tx_inclusion = transcript.get_read_counts_array(second_stop+3, -1*up, down, read_end=read_end,
-                                                                    read_lengths=read_lengths)
-                    normed_count_sum += tx_count/(float(cds_reads)/transcript.cds_length)
-                    inclusion_sum += tx_inclusion
-        nt_positions = np.arange(-1*up, down+1)-0.5
-        plot.bar(nt_positions, normed_count_sum/inclusion_sum,
-                  color=colormap((plot_index - 1) / float(len(experiment.libs))), lw=0, label=sample_name)
-        plot.set_title(sample_name, fontsize=8)
-        plot_index += 1
-        if plot_index == num_libs:
-            plot.set_xlabel("relative to second in-frame stop", fontsize=8)
-        plot.set_ylabel("average density\n (read %s end)" % (read_end), fontsize=8)
-        plot.set_xlim(-1*up, down)
-        minorLocator = AutoMinorLocator(10)
-        plot.xaxis.set_minor_locator(minorLocator)
-        plot.get_xaxis().set_tick_params(which='both', direction='out')
-        plot.get_yaxis().set_tick_params(which='both', direction='out')
-    #lg = plt.legend(loc=2, prop={'size': 12}, labelspacing=0.2)
-    #lg.draw_frame(False)
-    plt.tight_layout()
-    out_name = os.path.join(experiment.settings.get_rdir(), 'plots', 'second_stop_codon_avg_%s_%s.pdf' %(read_end, str(read_lengths)))
-    plt.savefig(out_name, transparent='True', format='pdf')
-    plt.clf()
-
 def plot_first_exon_average(experiment, up = 500, down = 100, min_cds_reads = 128, read_end='5p', read_lengths='all'):
     num_libs = len(experiment.libs)
     num_plots_wide = 1
@@ -334,19 +279,20 @@ def plot_first_exon_average(experiment, up = 500, down = 100, min_cds_reads = 12
         normed_count_sum = np.zeros(down+up+1)
         inclusion_sum = np.zeros(down + up + 1)
         for transcript in lib.transcripts.values():
-            if not transcript.get_first_jxn_in_CDS() == None:
-                if read_end == '5p':
-                    start_offset = -15
-                    stop_offset = -12
-                elif read_end == '3p':
-                    start_offset = 14
-                    stop_offset = 18
-                cds_reads = transcript.get_cds_read_count(start_offset, stop_offset, read_end=read_end, read_lengths=read_lengths)
-                if cds_reads >= min_cds_reads:
-                    tx_count, tx_inclusion = transcript.get_read_counts_array(transcript.get_first_jxn_in_CDS(), -1*up, down, read_end=read_end,
-                                                                    read_lengths=read_lengths)
-                    normed_count_sum += tx_count/(float(cds_reads)/transcript.cds_length)
-                    inclusion_sum += tx_inclusion
+            if transcript.is_coding:
+                if not transcript.get_first_jxn_in_CDS() == None:
+                    if read_end == '5p':
+                        start_offset = -15
+                        stop_offset = -12
+                    elif read_end == '3p':
+                        start_offset = 14
+                        stop_offset = 18
+                    cds_reads = transcript.get_cds_read_count(start_offset, stop_offset, read_end=read_end, read_lengths=read_lengths)
+                    if cds_reads >= min_cds_reads:
+                        tx_count, tx_inclusion = transcript.get_CDS_read_counts_array(transcript.get_first_jxn_in_CDS(), -1 * up, down, read_end=read_end,
+                                                                                      read_lengths=read_lengths)
+                        normed_count_sum += tx_count/(float(cds_reads)/transcript.cds_length)
+                        inclusion_sum += tx_inclusion
         nt_positions = np.arange(-1*up, down+1)-0.5
         plot.bar(nt_positions, normed_count_sum/inclusion_sum,
                   color=colormap((plot_index - 1) / float(len(experiment.libs))), lw=0, label=sample_name)
@@ -380,18 +326,19 @@ def plot_stop_positional_read_lengths(experiment, up = 500, down = 100, min_cds_
         length_sum = np.zeros(down+up+1)
         count_sum = np.zeros(down + up + 1)
         for transcript in lib.transcripts.values():
-            if read_end == '5p':
-                start_offset = -15
-                stop_offset = -12
-            elif read_end == '3p':
-                start_offset = 14
-                stop_offset = 18
-            cds_reads = transcript.get_cds_read_count(start_offset, stop_offset, read_end=read_end, read_lengths=read_lengths)
-            if cds_reads >= min_cds_reads:
-                length_sum_array, counts_array = transcript.get_avg_read_lengths_array(transcript.cds_end, -1*up, down,
-                                                                                       read_end=read_end)
-                length_sum += length_sum_array
-                count_sum += counts_array
+            if transcript.is_coding:
+                if read_end == '5p':
+                    start_offset = -15
+                    stop_offset = -12
+                elif read_end == '3p':
+                    start_offset = 14
+                    stop_offset = 18
+                cds_reads = transcript.get_cds_read_count(start_offset, stop_offset, read_end=read_end, read_lengths=read_lengths)
+                if cds_reads >= min_cds_reads:
+                    length_sum_array, counts_array = transcript.get_avg_read_lengths_array(transcript.cds_end, -1*up, down,
+                                                                                           read_end=read_end)
+                    length_sum += length_sum_array
+                    count_sum += counts_array
         nt_positions = np.arange(-1*up, down+1)
         plot.plot(nt_positions, length_sum/count_sum,
                   color=colormap((plot_index - 1) / float(len(experiment.libs))), lw=1, label=sample_name)
@@ -426,18 +373,19 @@ def plot_start_positional_read_lengths(experiment, up = 500, down = 100, min_cds
         length_sum = np.zeros(down+up+1)
         count_sum = np.zeros(down + up + 1)
         for transcript in lib.transcripts.values():
-            if read_end == '5p':
-                start_offset = -15
-                stop_offset = -12
-            elif read_end == '3p':
-                start_offset = 14
-                stop_offset = 18
-            cds_reads = transcript.get_cds_read_count(start_offset, stop_offset, read_end=read_end, read_lengths=read_lengths)
-            if cds_reads >= min_cds_reads:
-                length_sum_array, counts_array = transcript.get_avg_read_lengths_array(transcript.cds_start, -1*up, down,
-                                                                                       read_end=read_end)
-                length_sum += length_sum_array
-                count_sum += counts_array
+            if transcript.is_coding:
+                if read_end == '5p':
+                    start_offset = -15
+                    stop_offset = -12
+                elif read_end == '3p':
+                    start_offset = 14
+                    stop_offset = 18
+                cds_reads = transcript.get_cds_read_count(start_offset, stop_offset, read_end=read_end, read_lengths=read_lengths)
+                if cds_reads >= min_cds_reads:
+                    length_sum_array, counts_array = transcript.get_avg_read_lengths_array(transcript.cds_start, -1*up, down,
+                                                                                           read_end=read_end)
+                    length_sum += length_sum_array
+                    count_sum += counts_array
         nt_positions = np.arange(-1*up, down+1)
         plot.plot(nt_positions, length_sum/count_sum,
                   color=colormap((plot_index - 1) / float(len(experiment.libs))), lw=1, label=sample_name)
@@ -472,7 +420,7 @@ def plot_first_exon_positional_read_lengths(experiment, up = 500, down = 100, mi
         length_sum = np.zeros(down+up+1)
         count_sum = np.zeros(down + up + 1)
         for transcript in lib.transcripts.values():
-            if not transcript.get_first_jxn_in_CDS() == None:
+            if transcript.is_coding and not transcript.get_first_jxn_in_CDS() == None:
                 if read_end == '5p':
                     start_offset = -15
                     stop_offset = -12
@@ -485,10 +433,9 @@ def plot_first_exon_positional_read_lengths(experiment, up = 500, down = 100, mi
                                                                                            read_end=read_end)
                     length_sum += length_sum_array
                     count_sum += counts_array
-        nt_positions = np.arange(-1*up, down+1)
-        plot.plot(nt_positions, length_sum/count_sum,
-                  color=colormap((plot_index - 1) / float(len(experiment.libs))), lw=1, label=sample_name)
-        plot.set_title(sample_name, fontsize=8)
+            nt_positions = np.arange(-1*up, down+1)
+            plot.plot(nt_positions, length_sum/count_sum, color=colormap((plot_index - 1) / float(len(experiment.libs))), lw=1, label=sample_name)
+            plot.set_title(sample_name, fontsize=8)
         plot_index += 1
         if plot_index == num_libs:
             plot.set_xlabel("relative to CDS stop", fontsize=8)
@@ -505,3 +452,6 @@ def plot_first_exon_positional_read_lengths(experiment, up = 500, down = 100, mi
     out_name = os.path.join(experiment.settings.get_rdir(), 'plots', 'first_ej_lengths_%s_%s.pdf' %(read_end, str(read_lengths)))
     plt.savefig(out_name, transparent='True', format='pdf')
     plt.clf()
+
+def codon_metaplots(library):
+    pass
