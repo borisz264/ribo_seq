a8eab5052dc501be3125ac9327ff322a7053608a
boris <borzin264@gmail.com>
2019-11-01 21:22:56 -0400

    fized a_site mapping code

--- ribo_lib.py
+++ ribo_lib.py
@@ -303,8 +303,10 @@ class transcript:
         '''
         read_dict = {}
         super_dict = self.fragment_5p_lengths_at_position
-        for position in super_dict:
-            read_dict[position] = sum([super_dict[position+a_site_offsets[read_length]][read_length] for read_length in a_site_offsets if read_length in super_dict[position]])
+        for position in super_dict.keys():
+            read_dict[position] = sum(
+                [super_dict[position + a_site_offsets[read_length]][read_length] for read_length in a_site_offsets
+                 if read_length in super_dict[position + a_site_offsets[read_length]]])
         return read_dict
 
     def get_fractional_read_coverage(self, read_lengths = 'all'):
